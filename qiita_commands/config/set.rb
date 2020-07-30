55ce533b644ed5442e5d1f303121e3994a9fb5ea
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-30 13:35:39 +0900

    :sparkles: ConfigSettings クラスのテストを実装

--- qiita_commands/config/set.rb
+++ qiita_commands/config/set.rb
@@ -7,8 +7,9 @@ module QiitaCommands
       config = QiitaCommands::Config.new
 
       QiitaTrend.configure do |qiita_config|
-        qiita_config.user_name = config.user_name
-        qiita_config.password = config.password
+        qiita_config.user_name       = config.user_name
+        qiita_config.password        = config.password
+        qiita_config.cache_directory = config.cache_directory
       end
     end
   end
---
+++ spec/config/set_spec.rb
@@ -0,0 +1,24 @@
+require 'qiita_trend'
+require './spec/spec_helper'
+require './spec/support/config_yaml_helper.rb'
+require './qiita_commands/config/set.rb'
+
+module QiitaCommands
+  describe ConfigSettings do
+    describe '.perform' do
+      let(:qiita_trend_config) { QiitaTrend.configuration }
+
+      include_context 'mock config.yml'
+
+      subject { described_class.perform }
+
+      before { subject }
+
+      it 'qiita_trendにYamlファイルの内容が設定されていること' do
+        expect(qiita_trend_config.user_name).to eq(mock_yaml[:user_name])
+        expect(qiita_trend_config.password).to eq(mock_yaml[:password])
+        expect(qiita_trend_config.cache_directory).to eq(mock_yaml[:cache_directory])
+      end
+    end
+  end
+end
